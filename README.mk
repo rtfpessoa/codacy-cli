3a0e9b27eb5a6f6ceaf9c8ecf51278a872f5b38c
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-05-25 13:29:51 +0100

    add some documentation and follow node guidelines

---
+++ README.mk
@@ -0,0 +1,19 @@
+Codacy CLI, Automated Code Linting and Monitoring
+--------------------------------------------------
+
+\[ [Use it online](https://www.codacy.com) • [Feature](https://www.codacy.com/feature?????) •
+[Pricing](https://www.codacy.com/pricing) • [About us](https://www.codacy.com/about) •
+[GitHub](https://www.github.com) • [Blog](http://blog.codacy.com) •
+[Twitter](https://twitter.com/codacy/) \]
+
+Codacy is a ...
+
+
+#### License
+
+Codacy CLI is distributed under the MIT License.
+
+
+#### Thank you!
+
+We really appreciate all kind of feedback. Thanks for using Codacy!
